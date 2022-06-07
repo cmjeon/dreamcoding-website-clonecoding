86eca42f1ed5474c7895dbf7bc134f526cd18434
cmj <chmin82@gmail.com>
2022-06-08 07:41:33 +0900

    feat: Javascript:Fun dynamics
    - Handle contact me button

--- main.js
+++ main.js
@@ -22,6 +22,16 @@ navbarMenu.addEventListener('click', (event) => {
     return;
   }
   console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({behavior:'smooth'});
+  scrollIntoView(link);
 });
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact')
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior:'smooth' })
+}
