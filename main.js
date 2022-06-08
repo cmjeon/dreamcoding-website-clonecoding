0cc8a3dc7197e5f5d7dcf9dd13b259028df903b1
cmj <chmin82@gmail.com>
2022-06-09 08:43:34 +0900

    feat: Javascript:Fun dynamics
    - Navbar toggle button

--- main.js
+++ main.js
@@ -1,17 +1,6 @@
 'use strict'
 
 // Make navbar transparent when it is on the top
-const navbar = document.querySelector('#navbar');
-const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener('scroll', () => {
-  if(window.scrollY > navbarHeight) {
-    navbar.classList.add('navbar--dark');
-  } else {
-    navbar.classList.remove('navbar--dark');
-  }
-});
-
-// Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
   const target = event.target;
@@ -19,7 +8,27 @@ navbarMenu.addEventListener('click', (event) => {
   if(link == null) {
     return;
   }
+  navbarMenu.classList.remove('open');
   scrollIntoView(link);
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  if(window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+    navbarMenu.classList.remove('open');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+});
+
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open')
 });
 
 // Handle click on "contact me" button on home
