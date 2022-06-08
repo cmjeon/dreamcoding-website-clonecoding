226fdeeb797a417fe3d2173074b50c0dd86760d6
cmj <chmin82@gmail.com>
2022-06-09 07:44:11 +0900

    feat: Javascript:Fun dynamics
    - Final touch on button state

--- main.js
+++ main.js
@@ -59,6 +59,13 @@ workBtnContainer.addEventListener('click', (e) => {
   if(filter == null) {
     return
   }
+
+  // Remove selection from the previous item and select the new one
+  const active = document.querySelector('.category__btn.selected');
+  active.classList.remove('selected');
+  const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+  target.classList.add('selected');
+
   projectContainer.classList.add('animation-out');
   setTimeout(() => {
     projects.forEach((project) => {
